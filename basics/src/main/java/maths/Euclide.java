43995dbd2dcafe9fc72bf1d3b87cf60e17fb1e3e
matthcol <matthias.colin@gmail.com>
2023-04-04 08:51:29 +0200

    add comments

--- basics/src/main/java/maths/Euclide.java
+++ basics/src/main/java/maths/Euclide.java
@@ -1,7 +1,15 @@
 package maths;
 
 public class Euclide {
+
+    /**
+     * Compute the gcd of two natural integers
+     * @param a first number (must be > 0)
+     * @param b second number (must be > 0)
+     * @return the gcd of a and b
+     */
     public static int gcd(int a, int b){
+        // TODO: reject numbers <= 0
         while (a != b) {
             if (a > b) a =  a - b;
             else b = b - a;
