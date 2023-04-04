c357e8d96745ab04b03d5f0bb1877610de770824
matthcol <matthias.colin@gmail.com>
2023-04-04 09:24:41 +0200

    test with csvsource

--- basics/src/test/java/maths/EuclideTest.java
+++ basics/src/test/java/maths/EuclideTest.java
@@ -1,14 +1,18 @@
 package maths;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class EuclideTest {
 
-    @Test
-    void testGcd_withArgOne(){
-        int res = Euclide.gcd(1, 3);
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3, 4, 5, 7, 9})
+    void testGcd_withArgOne(int b){
+        int res = Euclide.gcd(1, b);
         assertEquals(1, res);
     }
 
@@ -16,6 +20,18 @@ class EuclideTest {
     void testGcd_15_21(){
         int res = Euclide.gcd(15,21);
         assertEquals(3, res);
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "15, 21, 3",
+            "21, 15, 3",
+            "12, 16, 4",
+            "16, 12, 4"
+    })
+    void testGcd(int a, int b, int gcdExpected){
+        int gcdComputed = Euclide.gcd(a, b);
+        assertEquals(gcdExpected, gcdComputed);
     }
 
 }
