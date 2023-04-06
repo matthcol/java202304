c419985992270505d4fc05f16a5aba912c455bde
matthcol <matthias.colin@gmail.com>
2023-04-06 10:44:28 +0200

    imlement methods distance, translate

--- geometry/src/main/java/geometry/model/Point.java
+++ geometry/src/main/java/geometry/model/Point.java
@@ -1,5 +1,37 @@
 package geometry.model;
 
-public class Point {
+import lombok.*;
 
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class Point {
+    private double x;
+    private double y;
+
+    /**
+     * distance from this point to other point
+     * @param other
+     * @return distance between this and other
+     */
+    public double distance(Point other) {
+        return Math.hypot(
+        		this.x - other.x, 
+        		this.y - other.y
+        );
+    }
+
+    /**
+     * translate this point with relative offsets
+     * @param deltaX x coordinate offset
+     * @param deltaY y coordinate offset
+     */
+	public void translate(double deltaX, double deltaY) {
+		this.x += deltaX;
+        this.y += deltaY;
+	}
+
+    
 }
