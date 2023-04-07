47322e7e31f34f14534eadd8da923a3df9d0a3d4
matthcol <matthias.colin@gmail.com>
2023-04-07 10:38:58 +0200

    SQL SELECT example

--- movies/src/test/java/sql/movie/MovieDatabaseDemo.java
+++ movies/src/test/java/sql/movie/MovieDatabaseDemo.java
@@ -1,5 +1,7 @@
 package sql.movie;
 
+import model.MovieL;
+import model.PgType;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -8,7 +10,9 @@ import sql.DatabaseTools;
 import javax.sql.DataSource;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -29,8 +33,38 @@ class MovieDatabaseDemo {
 
     @Test
     void testConnection() throws SQLException {
-        Connection connection = dataSource.getConnection();
-        System.out.println(connection);
+        try (Connection connection = dataSource.getConnection()) {
+            System.out.println(connection);
+            // Do something with the connection
+        } // connection.close(); // for all scenarii (normal, exception,...)
+    }
+
+    @Test
+    void demoReadFromDb() throws SQLException {
+        String sql = "SELECT id, title, year, duration, pg FROM movies WHERE year = 1984";
+        try (
+            Connection connection = dataSource.getConnection();
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(sql)
+        ) {
+            while (resultSet.next()) {
+                int id = resultSet.getInt("id");
+                String title = resultSet.getString("title");
+                int year = resultSet.getInt("year");
+                short duration = resultSet.getShort("duration");
+                String pgStr = resultSet.getString("pg");
+                PgType pg = PgType.valueOf(pgStr);
+//                System.out.println(id
+//                        + " " + title
+//                        + " " + year
+//                        + " " + duration
+//                        + " " + pgStr
+//                 );
+                MovieL movie = new MovieL(title, year, duration, pg);
+                System.out.println(movie);
+            }
+            System.out.println(resultSet);
+        } // close resultSet, statement, connection
     }
 
 
